fb4aef385b4be392a7045ceddcca619a82b5f1b3
Patrick <patrickwu@kinpo.com.tw>
2016-05-10 17:24:27 +0800

    Up to LabB6

--- LabB/LabB6.s
+++ LabB/LabB6.s
@@ -5,9 +5,12 @@ main:	#--------------------
 		syscall
 		add		$t0, $0, $v0	# t0=v0
 		addi	$t1, $0, 7		# t1=7
-		add		$t2, $t0, $t1	# t2=t0+t1
+		beq		$t0, $t1, XX
+		sub		$t2, $t0, $t1	# t2=t0-t1
+		j		YY
+XX:		add		$t2, $t0, $t1	# t2=t0+t1
 		#--------------------
-		addi	$v0, $0, 1		# service #1
+YY:		addi	$v0, $0, 1		# service #1
 		add		$a0, $0, $t2	# printInt
 		syscall					# do print
 		#--------------------
--- LabB/LabB6_1.s
+++ LabB/LabB6_1.s
@@ -5,9 +5,12 @@ main:	#--------------------
 		syscall
 		add		$t0, $0, $v0	# t0=v0
 		addi	$t1, $0, 7		# t1=7
+		bne		$t0, $t1, XX
 		add		$t2, $t0, $t1	# t2=t0+t1
+		j		YY
+XX:		sub		$t2, $t0, $t1	# t2=t0-t1
 		#--------------------
-		addi	$v0, $0, 1		# service #1
+YY:		addi	$v0, $0, 1		# service #1
 		add		$a0, $0, $t2	# printInt
 		syscall					# do print
 		#--------------------
