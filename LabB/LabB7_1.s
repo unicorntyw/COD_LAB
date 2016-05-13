dca29651cf2a096feba3a27de3d2732400124859
Patrick <patrickwu@kinpo.com.tw>
2016-05-13 17:24:39 +0800

    Up to LabB9

--- LabB/LabB7_1.s
+++ LabB/LabB7_1.s
@@ -6,11 +6,11 @@ main:	#--------------------
 		add		$t0, $0, $v0	# t0=v0
 		addi	$t1, $0, 7		# t1=7
 		slt		$t3, $t0, $t1	# t3=1 if t0 < t1
-		bne		$t3, $0, then	# t3=1
-		sub		$t2, $t0, $t1	# t2=t0-t1
+		beq		$t3, $0, else	# t3=0
+		add		$t2, $t0, $t1	# t2=t0+t1	
 		j		exit
 		#--------------------
-then:	add		$t2, $t0, $t1	# t2=t0+t1
+else:	sub		$t2, $t0, $t1	# t2=t0-t1
 exit:	addi	$v0, $0, 1		# service #1
 		add		$a0, $0, $t2	# printInt
 		syscall					# do print
