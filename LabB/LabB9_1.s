a7ad62616aed79150feb78faef610caeb7f5a2f5
Patrick <patrickwu@kinpo.com.tw>
2016-06-08 15:02:28 +0800

    Up to LabB9_1

--- LabB/LabB9_1.s
+++ LabB/LabB9_1.s
@@ -7,12 +7,11 @@ main:	#--------------------
 		add		$t0, $0,  $v0
 		li		$t5,	 0
 loop:	slt		$t1, $t5, $t0
-		beq		$t1, $0,  done
-		add		$s0, $s0, $t5
-		addi	$t5, 1
-		j		loop
-		#--------------------
-done:	addi	$v0, $0,  1		# service #1
+		bne		$t1, $0,  addn
+		addi	$v0, $0,  1		# service #1
 		add		$a0, $0, $s0	# printInt
 		syscall					# do print
 		jr		$ra				# return
+addn:	add		$s0, $s0, $t5
+		addi	$t5, 1
+		j		loop
