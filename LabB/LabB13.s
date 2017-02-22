975393ad2e986f6ac23c8500aa8ed9b2a8656bcb
Patrick <patrickwu@kinpo.com.tw>
2017-02-22 17:18:53 +0800

    finish Lab15 and create Lab16

--- LabB/LabB13.s
+++ LabB/LabB13.s
@@ -2,8 +2,8 @@
 main:	#-------------------
 		addi	$v0, $0, 5		# service #5 v0=readInt
 		syscall
-		add		$a0, $0, $v0	# t0=v0
-		sll		$a0, $a0, 21
+		add		$t0, $0, $v0	# t0=v0
+		sll		$a0, $t0, 21
 		srl		$a0, $a0, 31
 		addi	$v0, $0,  1		# service #1
 		syscall					# do print
